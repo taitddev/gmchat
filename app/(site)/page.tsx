f640cc06761e01e4ac1ba2cbb3b9347e13eb0780
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2023-05-11 17:50:55 +0700

    style: change css

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,18 +3,7 @@ import AuthForm from "./components/AuthForm";
 
 const Auth = () => {
   return (
-    <div 
-      className="
-        flex 
-        min-h-full 
-        flex-col 
-        justify-center 
-        py-12 
-        sm:px-6 
-        lg:px-8 
-        bg-gray-100
-      "
-    >
+    <div className="flex min-h-full flex-col justify-center bg-gray-100 py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           height="48"
@@ -23,22 +12,13 @@ const Auth = () => {
           src="/images/logo.png"
           alt="Logo"
         />
-        <h2 
-          className="
-            mt-6 
-            text-center 
-            text-3xl 
-            font-bold 
-            tracking-tight 
-            text-gray-900
-          "
-          >
-            Sign in to your account
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
         </h2>
       </div>
-      <AuthForm />      
-  </div>
-  )
-}
+      <AuthForm />
+    </div>
+  );
+};
 
 export default Auth;
