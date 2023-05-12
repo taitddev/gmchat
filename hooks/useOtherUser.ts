1f08552a7f60c019603b324a55b3377ae7970fb0
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2023-05-12 15:05:28 +0700

    fix: fix import type

--- hooks/useOtherUser.ts
+++ hooks/useOtherUser.ts
@@ -1,15 +1,19 @@
-import { useSession } from "next-auth/react";
 import { useMemo } from "react";
-import { FullConversationType } from "../types";
+import { useSession } from "next-auth/react";
 import { User } from "@prisma/client";
+import { FullConversationType } from "@/app/types";
 
-const useOtherUser = (conversation: FullConversationType | { users: User[] }) => {
+const useOtherUser = (
+  conversation: FullConversationType | { users: User[] }
+) => {
   const session = useSession();
 
   const otherUser = useMemo(() => {
     const currentUserEmail = session.data?.user?.email;
 
-    const otherUser = conversation.users.filter((user) => user.email !== currentUserEmail);
+    const otherUser = conversation.users.filter(
+      (user) => user.email !== currentUserEmail
+    );
 
     return otherUser[0];
   }, [session.data?.user?.email, conversation.users]);
