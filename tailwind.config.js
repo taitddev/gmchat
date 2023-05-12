e851b6e64a47e31c6712cab6b079f47f0d10ed65
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2023-05-12 11:41:41 +0700

    style: change css button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bluePrimary: "#1C79BD",
+        blueSecondary: "#5AA2D6",
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms')({
-      strategy: 'class'
-    })
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
   ],
-}
+};
