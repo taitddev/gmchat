b39d28f15c31cab0e09a46fee8a527c89efd5ff6
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2023-05-12 12:44:24 +0700

    feat: add mobile sidebar

---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+import clsx from "clsx";
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({
+  href,
+  icon: Icon,
+  active,
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return (
+    <Link
+      onClick={handleClick}
+      href={href}
+      className={clsx(
+        `group flex w-full justify-center gap-x-3 p-4 text-sm font-semibold leading-6 text-bluePrimary hover:bg-gray-100 hover:text-blueDarker`,
+        active && "bg-gray-100 text-black"
+      )}
+    >
+      <Icon className="h-6 w-6" />
+    </Link>
+  );
+};
+
+export default MobileItem;
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: ReactNode }) {
   const currentUser = await getCurrentUser();
@@ -8,6 +9,7 @@ async function Sidebar({ children }: { children: ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar currentUser={currentUser!} />
+      <MobileFooter />
       <main className="h-full lg:pl-20">{children}</main>
     </div>
   );
