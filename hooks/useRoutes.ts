0f2396164270473316237e1988804819b60811d6
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2023-05-12 11:27:08 +0700

    feat: add custom hook

--- hooks/useRoutes.ts
+++ hooks/useRoutes.ts
@@ -1,10 +1,28 @@
 import { useMemo } from "react";
-import { HiArrowLeftOnRectangle } from "react-icons/hi2";
+import { usePathname } from "next/navigation";
+import { HiChat } from "react-icons/hi";
+import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
 import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
 
 const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+
   const routes = useMemo(
     () => [
+      {
+        label: "Chat",
+        href: "/conversations",
+        icon: HiChat,
+        active: pathname === "/conversations" || !!conversationId,
+      },
+      {
+        label: "Users",
+        href: "/users",
+        icon: HiUsers,
+        active: pathname === "/users",
+      },
       {
         label: "Logout",
         onClick: () => signOut(),
@@ -12,7 +30,7 @@ const useRoutes = () => {
         icon: HiArrowLeftOnRectangle,
       },
     ],
-    []
+    [pathname, conversationId]
   );
 
   return routes;
