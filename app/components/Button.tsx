695058ca6a770b02a4ff17d24d3aebe176a46a43
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2023-05-12 15:18:40 +0700

    styles: change button css

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -25,18 +25,7 @@ const Button: React.FC<ButtonProps> = ({
       type={type}
       disabled={disabled}
       className={clsx(
-        `
-        flex 
-        justify-center 
-        rounded-md 
-        px-3 
-        py-2 
-        text-sm 
-        font-semibold 
-        focus-visible:outline 
-        focus-visible:outline-2 
-        focus-visible:outline-offset-2 
-        `,
+        `flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
         disabled && "cursor-default opacity-50",
         fullWidth && "w-full",
         secondary ? "text-gray-900" : "text-white",
@@ -44,7 +33,7 @@ const Button: React.FC<ButtonProps> = ({
           "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
         !secondary &&
           !danger &&
-          "bg-blueSecondary hover:bg-bluePrimary focus-visible:outline-bluePrimary"
+          "bg-bluePrimary hover:bg-blueDarker focus-visible:outline-blueDarker"
       )}
     >
       {children}
